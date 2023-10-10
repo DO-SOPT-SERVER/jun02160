f1af03694e112c382dcc4c17f8e57865f3563aa8
jun02160 <jun020216@naver.com>
2023-10-10 12:24:12 +0900

    프로젝트 초기 세팅

---
+++ WEEK1/kotlin-seminar/src/main/kotlin/sopt/org/kotlinseminar/KotlinSeminarApplication.kt
@@ -0,0 +1,11 @@
+package sopt.org.kotlinseminar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSeminarApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSeminarApplication>(*args)
+}
