632d4beaf9cca7ba66e57de4340d2d84e0a07729
jun02160 <jun020216@naver.com>
2023-10-13 21:09:44 +0900

    [1주차] 스프링부트 프로젝트 Java -> Kotlin 변환

---
+++ WEEK1/kotlin-seminar/src/main/kotlin/sopt/org/kotlinseminar/service/Person.kt
@@ -0,0 +1,12 @@
+package sopt.org.kotlinseminar.service
+
+import lombok.AccessLevel
+import lombok.Builder
+import lombok.NoArgsConstructor
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+class Person(
+    var lastName: String?,
+    var firstName: String?
+)   // 자바의 멤버 대신 프로퍼티 형식으로 작성하면 Getter, Setter의 역할까지 한번에 처리해준다
