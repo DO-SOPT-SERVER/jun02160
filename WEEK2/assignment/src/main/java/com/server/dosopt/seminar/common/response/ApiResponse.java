0d4692ef5fde6c50eda00b87127d1dff46e42ab1
jun02160 <jun020216@naver.com>
2023-10-21 22:54:04 +0900

    [FEAT] 공통 응답 DTO 객체 생성

---
+++ WEEK2/assignment/src/main/java/com/server/dosopt/seminar/common/response/ApiResponse.java
@@ -0,0 +1,56 @@
+package com.server.dosopt.seminar.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+import org.springframework.http.HttpStatus;
+
+/**
+ * TODO 공통 Response 클래스 -> Record 타입으로 변경할 것
+ * @param <T>
+ */
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonPropertyOrder({"code", "status", "success", "data"})
+public class ApiResponse<T> {
+
+    private final int code;
+    private final String status;
+    private final boolean success;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    // 성공
+    public static ApiResponse success(HttpStatus httpStatus) {
+        return ApiResponse.builder()
+                .code(httpStatus.value())
+                .status(httpStatus.getReasonPhrase())
+                .success(true)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success(HttpStatus httpStatus, T data) {
+        return ApiResponse.<T>builder()
+                .code(httpStatus.value())
+                .status(httpStatus.getReasonPhrase())
+                .success(true)
+                .data(data)
+                .build();
+    }
+
+    public static ApiResponse error(HttpStatus httpStatus) {
+        return ApiResponse.builder()
+                .code(httpStatus.value())
+                .status(httpStatus.getReasonPhrase())
+                .success(false)
+                .build();
+    }
+
+}
