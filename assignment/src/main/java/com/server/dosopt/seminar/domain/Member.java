fbf710d42f80639878900d9f4a2d53faa65f4501
jun02160 <jun020216@naver.com>
2023-11-07 20:48:48 +0900

    [RENAME] 과제코드 폴더 이동

---
+++ assignment/src/main/java/com/server/dosopt/seminar/domain/Member.java
@@ -0,0 +1,32 @@
+package com.server.dosopt.seminar.domain;
+
+import jakarta.persistence.*;   // java 17버전 이상부터는 jakarta로 변경
+import lombok.AccessLevel;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)   // 엔티티에는 반드시 고유 식별자(ID)가 존재해야 핢 -> 생성전략을 함께 지정
+    private Long id;
+    private String name;
+    private String nickname;
+    private int age;
+
+    @Embedded   // 값 타입을 사용하는 곳에 붙이는 어노테이션
+    private SOPT sopt;
+
+    @Builder   // 빌더패턴을 적용하여 객체 생성 가능
+    public Member(String name, String nickname, int age, SOPT sopt) {
+        this.name = name;
+        this.nickname = nickname;
+        this.age = age;
+        this.sopt = sopt;
+    }
+
+}
---
+++ assignment/src/main/java/com/server/dosopt/seminar/service/MemberService.java
@@ -0,0 +1,75 @@
+package com.server.dosopt.seminar.service;
+
+import com.server.dosopt.seminar.domain.Member;
+import com.server.dosopt.seminar.dto.request.MemberCreateRequest;
+import com.server.dosopt.seminar.dto.request.MemberProfileUpdateRequest;
+import com.server.dosopt.seminar.dto.response.MemberGetResponse;
+import com.server.dosopt.seminar.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)   // 클래스보다 메서드 레벨의 @Transactional이 우선적으로 적용 => 메서드 단에 붙여주면 읽기전용의 트랜잭션 X
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 조회
+    public MemberGetResponse getMemberByIdV1(Long memberId) {
+        return MemberGetResponse.of(findMemberById(memberId));
+    }
+
+    public MemberGetResponse getMemberByIdV2(Long memberId) {
+        return MemberGetResponse.of(memberRepository.findByIdOrThrow(memberId));
+    }
+
+    public List<MemberGetResponse> getMembers() {
+        return memberRepository.findAll()
+                .stream()
+                .map(MemberGetResponse::of)
+                .collect(Collectors.toList());
+    }
+
+
+    // 생성
+    @Transactional
+    public String create(MemberCreateRequest request) {
+        Member member = Member.builder()
+                .name(request.name())
+                .nickname(request.nickname())
+                .age(request.age())
+                .sopt(request.sopt())
+                .build();
+        return memberRepository.save(member).getId().toString();
+    }
+
+    // 수정
+    @Transactional
+    public void updateSOPT(Long memberId, MemberProfileUpdateRequest request) {
+        Member findMember = findMemberById(memberId);
+        findMember.getSopt().updateSopt(request.generation(), request.part());
+        memberRepository.save(findMember);
+
+    }
+
+    // 삭제
+    @Transactional
+    public void deleteMember(Long memberId) {
+        memberRepository.deleteById(memberId);
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+    }
+
+
+
+}
