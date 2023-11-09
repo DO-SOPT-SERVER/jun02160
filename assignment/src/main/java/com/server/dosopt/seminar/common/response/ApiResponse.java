ae0bb13088abe2a8c648bf18d412ffb3bdaaa6c1
jun02160 <jun020216@naver.com>
2023-11-09 16:49:05 +0900

    [FEAT] 201 응답용 내부 클래스 추가

--- assignment/src/main/java/com/server/dosopt/seminar/common/response/ApiResponse.java
+++ assignment/src/main/java/com/server/dosopt/seminar/common/response/ApiResponse.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.server.dosopt.seminar.enums.ErrorMessage;
 import com.server.dosopt.seminar.enums.SuccessMessage;
+import java.net.URI;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.AccessLevel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 /**
  * TODO 공통 Response 클래스 -> Record 타입으로 변경할 것
@@ -44,6 +47,35 @@ public class ApiResponse<T> {
                 .success(true)
                 .data(data)
                 .build();
+    }
+
+
+
+    @Builder
+    public static class CreatedApiResponse extends ResponseEntity {
+
+        private BodyBuilder bodyBuilder;
+
+        private final int code;
+        private final String status;
+        private final boolean success;
+        private CreatedApiResponse(BodyBuilder bodyBuilder, int code, String status, boolean success) {
+            super(HttpStatus.CREATED);
+            this.bodyBuilder = bodyBuilder;
+            this.code = code;
+            this.status = status;
+            this.success = success;
+        }
+
+        // 201 Createrd
+        public static CreatedApiResponse success(SuccessMessage successMessage, URI location) {
+            return CreatedApiResponse.builder()
+                    .bodyBuilder(created(location))
+                    .code(successMessage.getHttpStatusCode())
+                    .status(successMessage.getMessage())
+                    .success(true)
+                    .build();
+        }
     }
 
     public static ApiResponse error(ErrorMessage errorMessage) {
