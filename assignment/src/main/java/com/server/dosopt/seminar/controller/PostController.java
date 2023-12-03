4f862834d9f1e9202d01f7eac6855e535add1a48
jun02160 <jun020216@naver.com>
2023-12-04 00:18:50 +0900

    [3주차] PostController 리뷰 사항 반영

--- assignment/src/main/java/com/server/dosopt/seminar/controller/PostController.java
+++ assignment/src/main/java/com/server/dosopt/seminar/controller/PostController.java
@@ -15,6 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,34 +35,41 @@ public class PostController {
     private final PostService postService;
 
     private static final String CUSTOM_AUTH_ID = "X-Auth-id";  // 상수(static final)로 관리
+    private static final String POST_REQUEST_URI = "/api/post";
+
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse createPost(@RequestHeader(CUSTOM_AUTH_ID) Long memberId,
                                     @RequestBody PostCreateRequest request, HttpServletResponse response) {
         String postId = postService.create(request, memberId);
-        URI location = URI.create("/api/post" + postId);
+        URI location = URI.create(POST_REQUEST_URI + postId);
         response.setHeader("Location", location.toString());  // 응답 헤더에 Location 값 설정
         return ApiResponse.success(CREATE_POST_SUCCESS);
     }
 
     @GetMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
     public ApiResponse<PostGetResponse> getPostById(@PathVariable Long postId) {
         return ApiResponse.success(GET_POST_INFO_SUCCESS, postService.getById(postId));
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public ApiResponse<List<PostGetResponse>> getPosts(@RequestHeader(CUSTOM_AUTH_ID) Long memberId){
         return ApiResponse.success(GET_POST_LIST_SUCCESS, postService.getPosts(memberId));
     }
 
     @PatchMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
     public ApiResponse updatePost(@PathVariable Long postId,
-                                            @RequestBody PostUpdateRequest request) {
+                                    @RequestBody PostUpdateRequest request) {
         postService.update(request, postId);
         return ApiResponse.success(UPDATE_POSt_SUCCESS);
     }
 
     @DeleteMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
     public ApiResponse deletePost(@PathVariable Long postId) {
         postService.delete(postId);
         return ApiResponse.success(DELETE_MEMBER_SUCCESS);
